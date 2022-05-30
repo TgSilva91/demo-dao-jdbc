4013fb9833de9421624c05a228669dc1db4a7e9c
Tiago Silva <tgsilva1991@gmail.com>
2022-05-31 00:20:35 +0100

    DepartmentDaoJDBC deleteById implementation

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -15,12 +15,12 @@ public class Program2 {
 		Scanner sc = new Scanner(System.in);
 		
 		DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
-		/*
+		
 		System.out.println("==== Test 1: department insert ====");
 		Department newDepartment = new Department(null, "D2");
 		departmentDao.insert(newDepartment);
 		System.out.println("Inserted! New id = " + newDepartment.getId());
-		*/
+		
 		
 		System.out.println();
 		System.out.println("==== Test 2: department findById ====");
@@ -40,6 +40,13 @@ public class Program2 {
 		dep = departmentDao.findById(6);
 		dep.setName("Music");
 		departmentDao.update(dep);
+		
+		System.out.println();
+		System.out.println("==== Test 5: department delete ====");
+		System.out.print("Enter id for delect test: ");
+		int id = sc.nextInt();
+		departmentDao.deleteById(id);
+		System.out.println("Department deleted");
 		
 		sc.close();
 	}
